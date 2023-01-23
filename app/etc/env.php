cfd99c20d0be1dac9f31f7939f791715d9ad0924
Divyanshu kumar <17devdiv@gmail.com>
2023-01-23 18:03:59 +0530

    change in gitignore

---
+++ app/etc/env.php
@@ -0,0 +1,98 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'checkout' => [
+        'async' => 0,
+        'deferred_total_calculating' => 0
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'db' => [
+        'connection' => [
+            'indexer' => [
+                'host' => 'localhost',
+                'dbname' => 'magento2divya',
+                'username' => 'magento2divya',
+                'password' => 'Xkdx3tgkC977eHG',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'persistent' => null
+            ],
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento2divya',
+                'username' => 'magento2divya',
+                'password' => 'Xkdx3tgkC977eHG',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ],
+        'table_prefix' => ''
+    ],
+    'crypt' => [
+        'key' => '96395937f532cdd16687fe4472279354'
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'd45_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'd45_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'target_rule' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'magento2divya.24livehost.com'
+    ],
+    'install' => [
+        'date' => 'Tue, 16 Aug 2022 10:20:10 +0000'
+    ]
+];
